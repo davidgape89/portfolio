8a2c177c7d197321c0bdf2b34a3b31445c797325
davidgape89 <davidgape89@gmail.com>
2019-07-27 23:11:21 +0200

    Added deployment process

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Portfolio from './pages/portfolio/Portfolio';
 
 import './App.scss';
 
+// TODO - Include credit to the author of the favicon
+// <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
 const App = () => (
   <div className="App">
     <div className="content">
--- src/assets/mocks/article.js
+++ src/assets/mocks/article.js
@@ -1,5 +1,5 @@
 export default {
-  title: 'First ever Article',
+  title: 'Under construction',
   content: '# Hello world \n This is my first article in this website',
   creationDate: 1564234864671,
   lastModified: 1564234864671,
