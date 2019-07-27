afe32a244e9d38f6109419dcf8b21fc66cc17070
davidgape89 <davidgape89@gmail.com>
2019-07-27 16:44:27 +0200

    Added circleCI and firebase config

--- src/assets/mocks/article.js
+++ src/assets/mocks/article.js
@@ -2,4 +2,5 @@ export default {
   title: 'First ever Article',
   content: '# Hello world \n This is my first article in this website',
   creationDate: 1564234864671,
-}
+  lastModified: 1564234864671,
+};
