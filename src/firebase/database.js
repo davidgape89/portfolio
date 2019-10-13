ebb2b6f86e3a41fec89eb2006e77c234a63077e5
David Garcia Perez <davidgape89@gmail.com>
2019-10-13 23:09:16 +0200

    Fixed eslint errors

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -2,13 +2,17 @@ import {database} from './firebase';
 
 /**
  * Makes a request to create a new post
+ * @param {string} title  Title of the post
+ * @param {string} content Content of the post (mark-down)
+ * @param {string} authorUid Author of the post
+ * @return {Promise} Promise of when the post have been posted.
  */
 export function newPost(
-  title,
-  content,
-  authorUid,
+    title,
+    content,
+    authorUid,
 ) {
-  let postData = {
+  const postData = {
     title,
     content,
     creationDate: new Date().getTime(),
@@ -16,14 +20,14 @@ export function newPost(
   };
 
   return getPosts().then((posts) => {
-    if(posts) {
+    if (posts) {
       return database.ref('/posts').set([
         ...posts,
-        postData
+        postData,
       ]);
     } else {
       return database.ref('/posts').set([
-        postData
+        postData,
       ]);
     }
   });
