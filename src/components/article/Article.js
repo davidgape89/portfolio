0e14dd198caceaa922c875c9e43807075adc6140
David Garcia Perez <david@localhost.localdomain>
2019-10-21 00:04:58 +0200

    Made code block component a default export

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { CodeBlock } from '../code-block/CodeBlock';
+import CodeBlock from '../code-block/CodeBlock';
 
 import './Article.scss';
 
